e288eb632bbde6c16bdb2c9b94ec08f3dff74d14
Louiixx-h <louix.sm@gmail.com>
2021-06-25 20:42:16 -0300

    first commit

---
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -0,0 +1,36 @@
+package br.com.alura.leilao.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+
+@Service
+public class FinalizarLeilaoService {
+
+	@Autowired
+	private LeilaoDao leiloes;
+
+	public void finalizarLeiloesExpirados() {
+		List<Leilao> expirados = leiloes.buscarLeiloesExpirados();
+		expirados.forEach(leilao -> {
+			Lance maiorLance = maiorLanceDadoNoLeilao(leilao);
+			leilao.setLanceVencedor(maiorLance);
+			leilao.fechar();
+			leiloes.salvar(leilao);
+		});
+	}
+
+	private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
+		List<Lance> lancesDoLeilao = leilao.getLances();
+		lancesDoLeilao.sort((lance1, lance2) -> {
+			return lance2.getValor().compareTo(lance1.getValor());
+		});
+		return lancesDoLeilao.get(0);
+	}
+	
+}
---
+++ src/main/java/br/com/alura/leilao/service/GeradorDePagamento.java
@@ -0,0 +1,24 @@
+package br.com.alura.leilao.service;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Pagamento;
+
+@Service
+public class GeradorDePagamento {
+
+	@Autowired
+	private PagamentoDao pagamentos;
+
+	public void gerarPagamento(Lance lanceVencedor) {
+		LocalDate vencimento = LocalDate.now().plusDays(1);
+		Pagamento pagamento = new Pagamento(lanceVencedor, vencimento);
+		this.pagamentos.salvar(pagamento);
+	}
+
+}
